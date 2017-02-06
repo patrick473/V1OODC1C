a9e0cc9914e66cb6e2f5fa315753259c4e52d0c5
patrick kottman <patkottman@gmail.com>
2017-02-06 14:05:15 +0100

    week 1 les 1 gefixt

---
+++ week1/les1/opdracht2/Main.java
@@ -0,0 +1,24 @@
+package week1.les1.opdracht2;
+
+public class Main {
+	public static void main(String[] arg) {
+		String Naam1 = "Jan";
+		String Adres1 = "Nijenoord 1";
+		String Plaats1 = "Utrecht";
+		String Naam2 = "Wim";
+		String Adres2 = "Oudenoord 340";
+		String Plaats2 = "Utrecht";
+		
+		System.out.println("De gegevens van nr1 zijn:");
+		System.out.println(Naam1);
+		System.out.println(Adres1);
+		System.out.println(Plaats1);
+		
+		System.out.println("");
+		
+		System.out.println("De gegevens van nr2 zijn:");
+		System.out.println(Naam2);
+		System.out.println(Adres2);
+		System.out.println(Plaats2);
+	}
+}
