889a0b49af318950ff226ad2d7f45afc789e3301
patrick kottman <patkottman@gmail.com>
2017-02-07 16:12:52 +0100

    Add files via upload

---
+++ zelfstudie/zelfstudie1.java
@@ -0,0 +1,12 @@
+package zelfstudie;
+
+/**
+ * Created by patrick on 2/7/2017.
+ */
+public class zelfstudie1 {
+    public static void main(String[] args){
+        for(int i=1; i<11; i++){
+            System.out.println("Count is: " + i);
+        }
+    }
+}
---
+++ zelfstudie/zelfstudie2.java
@@ -0,0 +1,14 @@
+package zelfstudie;
+
+/**
+ * Created by patrick on 2/7/2017.
+ */
+public class zelfstudie2 {
+    public static void main(String[] args){
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Cont is: " + count);
+            count++;
+        }
+    }
+}
