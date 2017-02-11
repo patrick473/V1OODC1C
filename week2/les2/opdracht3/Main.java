8c366eeb6e245b2057d15c6c4aab3a3c6f875516
patrick kottman <patkottman@gmail.com>
2017-02-11 11:25:57 +0100

    Add files via upload

--- week2/les2/opdracht3/Main.java
+++ week2/les2/opdracht3/Main.java
@@ -1,17 +1,27 @@
 package week2.les2.opdracht3;
 import java.util.Scanner;
+import java.util.ArrayList;
 /**
  * Created by patrick on 2/7/2017.
  */
 public class Main {
-    public String naam;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Voer uw naam in: ");
-        String naam = scanner.nextLine();
-        System.out.println(naam);
-        if (naam == "0"){
-        scanner.close();}
-        System.out.println(naam);
+        Scanner scanner = new Scanner(System.in); //scanner koppelen aan toetsenbord
+        String d;
+        ArrayList<Double> l = new ArrayList<Double>();
+        do {
+            System.out.print("Voer double in: ");
+            d = scanner.nextLine(); // scanner wacht op invoer + enter
+            double d2 = Double.parseDouble(d);
+            l.add(d2);
+        } while (!d.equals("0"));
+        scanner.close();
+        double tot = 0.0;
+        for (double item : l) {
+            tot += item;
+            System.out.println(item);
+        }
+        double gem = tot / l.size();
+        System.out.println(gem);
     }
 }
